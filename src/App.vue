c3624df44518acb73eacebddb7a856960d4986af
Toly <tolerious.feng@gmail.com>
2024-07-28 12:49:14 +0800

    💄 Update styles.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex flex-col h-full">
-    <header class="bg-red-100 h-[60px]" v-if="isHeaderFooterVisible"></header>
-    <main class="flex-1 max-h-full"><RouterView /></main>
-    <footer class="bg-blue-300 h-[50px]" v-if="isHeaderFooterVisible"></footer>
+  <div class="flex flex-col" :class="[isHeaderFooterVisible ? '' : 'h-full']">
+    <header class="bg-red-100 h-16 fixed w-full grid" v-if="isHeaderFooterVisible">Header</header>
+    <main class="flex-1 max-h-full" :class="[isHeaderFooterVisible ? 'mt-16' : '']">
+      <RouterView />
+    </main>
+    <footer class="bg-blue-300 h-12" v-if="isHeaderFooterVisible">Footer</footer>
   </div>
 </template>
 
