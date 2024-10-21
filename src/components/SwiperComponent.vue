a75469185b818b6296d81e97c45fcbe7fbda3f03
Toly <toly.feng@gmail.com>
2024-10-21 22:18:09 +0800

    🚧 Add slider

---
+++ src/components/SwiperComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <carousel :items-to-show="1" :autoplay="4000" :wrap-around="true">
+    <slide v-for="slide in 10" :key="slide">
+      <div class="bg-slate-300">
+        <img src="@/assets/images/slider-01.png" alt="" srcset="" />
+      </div>
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <!-- <pagination /> -->
+    </template>
+  </carousel>
+</template>
+
+<script setup lang="ts">
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+</script>
