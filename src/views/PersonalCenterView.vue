a4ae7ac404ab7b6d8d625d06819bfbad04679f3e
Toly <tolerious.feng@kuka.com>
2024-11-21 15:12:40 +0800

    🚧 Change left column in personal center page.

--- src/views/PersonalCenterView.vue
+++ src/views/PersonalCenterView.vue
@@ -1,21 +1,26 @@
 <template>
   <div container class="lg:grid lg:grid-rows-1 lg:grid-cols-[200px_1fr] h-full">
     <div class="left-bar border-r-[1px]">
-      <div class="flex justify-around">
+      <!-- <div class="flex justify-around">
         <button
           @click="convertVideoHandler"
           class="rounded-sm bg-pink-600 px-2 py-1 text-white border-pink-600 m-2 cursor-pointer active:shadow-md active:shadow-pink-800 select-none"
         >
           转换所有视频
         </button>
-      </div>
-      <div class="flex justify-around">
+      </div> -->
+      <!-- <div class="flex justify-around">
         <button
           @click="navigateReciteWord"
           class="rounded-sm bg-pink-600 px-2 py-1 text-white border-pink-600 m-2 cursor-pointer active:shadow-md active:shadow-pink-800 select-none"
         >
           我要背单词
         </button>
+      </div> -->
+      <div
+        class="border-b-pink-500 border-b-[1px] border-dashed h-10 flex items-center justify-around"
+      >
+        <span>Toly 您好！</span>
       </div>
     </div>
     <div class="flex flex-col items-center bg- py-2 max-h-full overflow-scroll">
